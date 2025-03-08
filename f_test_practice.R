469d926aa23d7a8c56f7466bb18971067b049a9c
scottstossel <scottstossel1@gmail.com>
2025-03-08 03:14:00 -0500

    f test practice file

---
+++ f_test_practice.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+d <- data.frame(
+  x_1 = runif(1000),
+  x_2 = runif(1000),
+  x_3 = runif(1000),
+  x_4 = runif(1000)) %>%
+  mutate(
+    y = -1 + 1*x_1 + 2*x_2 + 0*x_3 + 0*x_4 + rnorm(1000)
+  )
+
+model_short <- lm(y ~ 1, data = d)
+model_long_a <- lm(y ~ 1 + x_1 + x_2, data=d)
+model_long_b <- lm(y ~ 1 + x_1 + x_2 + x_3 + x_4, data=d)
+
+anova(model_long_a, model_short, test = "F")
+anova(model_long_b, model_short, test="F")
+
+anova(model_long_b, model_long_a, test="F")
