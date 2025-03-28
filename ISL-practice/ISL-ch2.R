5a0ad129324d1f820475c2981325aeafd70e56b2
scottstossel <scottstossel1@gmail.com>
2025-03-27 23:19:46 -0600

    indexing practice

--- ISL-practice/ISL-ch2.R
+++ ISL-practice/ISL-ch2.R
@@ -32,3 +32,47 @@ sd(y)
 
 #graphics
 
+x <- rnorm(100)
+y <- rnorm(100)
+plot(x, y)
+plot(x, y, xlab = "this is the x-axis",
+     ylab = "this is the y-axis", main = "Plot of X vs Y")
+pdf("Figure.pdf")
+plot(x, y, col = "green")  
+dev.off()
+
+
+x <- seq(1, 10)
+x
+
+x <- 1:10
+x
+x <- seq(-pi, pi, length = 50)
+x
+
+y <- x
+f <- outer(x, y, function(x, y) cos(y) / (1 + x^2))
+contour(x, y, f)
+contour(x, y, f, nlevels = 45, add = T)
+fa <- (f - t(f)) / 2
+contour(x, y, fa, nlevels = 15)
+
+image(x, y, fa)
+persp(x, y, fa)
+persp(x, y, fa, theta = 30)
+persp(x, y, fa, theta = 30, phi = 20)
+persp(x, y, fa, theta = 30, phi = 70)
+persp(x, y, fa, theta = 30, phi = 40)
+
+#indexing data
+A <- matrix(1:16, 4, 4)
+A
+A[2,3]
+A[c(1, 3), c(2,4)]
+A[1:3, 2:4]
+A[1:2, ]
+
+A[, 1:2]
+A[1, ]
+A[-c(1,3), ]
+dim(A)
