19478b782e14ec54f1e8ad20c6bed16d15c24c04
scottstossel <scottstossel1@gmail.com>
2025-04-24 23:18:06 -0400

    non-linear transformations

--- ISL-practice/ISL-Ch3.R
+++ ISL-practice/ISL-Ch3.R
@@ -39,3 +39,32 @@ plot(predict(lm.fit), rstudent(lm.fit))
 
 plot(hatvalues(lm.fit))
 which.max(hatvalues(lm.fit))
+
+lm.fit <- lm(medv ~ lstat + age, data = Boston)
+summary(lm.fit)
+
+lm.fit <- lm(medv ~ ., data = Boston)
+summary(lm.fit)
+
+library(car)
+vif(lm.fit)
+lm.fit1 <- lm(medv ~ . - age, data = Boston)
+summary(lm.fit1)
+
+###Interaction terms
+summary(lm(medv ~ lstat * age, data = Boston))
+
+#Non-linear transformations
+lm.fit2 <- lm(medv ~ lstat + I(lstat^2))
+summary(lm.fit2)
+
+lm.fit <- lm(medv ~ lstat)
+anova(lm.fit, lm.fit2)
+
+par(mfrow = c(2,2))
+plot(lm.fit2)
+
+lm.fit5 <- lm(medv ~ poly(lstat, 5))
+summary(lm.fit5)
+
+summary(lm(medv ~ log(rm), data = Boston))
