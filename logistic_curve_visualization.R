075809b89f64afe42fba454f70974ab5e351f340
scottstossel <scottstossel1@gmail.com>
2025-05-11 17:07:00 -0400

    visualize logistic curve

---
+++ logistic_curve_visualization.R
@@ -0,0 +1,15 @@
+par(mfrow = c(1,2))
+
+beta0 <- 1
+beta1 <- 0.5
+curve(expr = exp(beta0 + beta1*x)/(1 + exp(beta0 + beta1*x)),
+  xlim = c(-15,15), col = 'black', main = expression(pi
+  == frac(e^{1+0.5*x[1]}, 1+e^{1+0.5*x[1]})), xlab = 
+  expression(x[1]), ylab = expression(pi))
+
+beta0 <- 1
+beta1 <- -0.5
+curve(expr = exp(beta0 + beta1*x)/(1 + exp(beta0 + beta1*x)),
+  xlim = c(-15,15), col = 'black', main = expression(pi
+  == frac(e^{1+0.5*x[1]}, 1+e^{1+0.5*x[1]})), xlab = 
+  expression(x[1]), ylab = expression(pi))
